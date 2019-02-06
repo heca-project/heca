b94367b0a31e9b8a9c20361b7446f1abc113d1a2
I picked a name <ipickedaname@github.com>
2019-02-06 08:22:54 +0000

    Added the converter cli.
    Fixed errors.

--- src/bin/heca.rs
+++ src/bin/heca.rs
@@ -1,11 +1,126 @@
+extern crate atoi;
+extern crate chrono;
+extern crate chrono_english;
 extern crate heca;
-extern crate chrono; 
-extern crate time; 
-use heca::*;
+extern crate time;
+
+#[macro_use]
+extern crate clap;
+use clap::App;
+
+use atoi::atoi;
 use chrono::prelude::*;
+use heca::*;
 
 fn main() {
-    println!("Year: {:?}", HebrewDate::from(Utc.ymd(2019, 1, 1).and_hms(23, 0, 0)));
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    if let Some(command) = matches.subcommand_matches("convert") {
+        if let Err(e) = convert(command.value_of("date").unwrap()) {
+            match e {
+                InputError::DateFormatError => println!("I can't parse the date you gave"),
+                InputError::TooManyMonths => println!("I need only one hebrew month"),
+                InputError::NotEnoughMonths => println!("I need one hebrew month"),
+                InputError::WrongAmntHebDateOptions => println!("I need something looking like Month-Day-Year or Day-Month-Year (For example: 10 Shvat 5710)."),
+                InputError::ConversionError(s) => println!("{}",s),
+            }
+        }
+    }
 }
 
-    
+fn convert(date_str: &str) -> Result<(), InputError> {
+    if let Ok(heb_date) = convert_date_to_fuzzy_hebrew(date_str) {
+        let eng_date = HebrewDate::to_eng(heb_date).map_err(|x| InputError::ConversionError(x))?;
+        println!("{}", eng_date);
+        Ok(())
+    } else if let Ok(eng_date) = convert_date_to_fuzzy_gregorian(&date_str) {
+        let heb_date =
+            HebrewDate::from_eng(eng_date).map_err(|x| InputError::ConversionError(x))?;
+        println!(
+            "{} {} {}",
+            heb_date.day(),
+            heb_date.month(),
+            heb_date.year()
+        );
+        Ok(())
+    } else {
+        Err(InputError::DateFormatError)
+    }
+}
+
+fn convert_date_to_fuzzy_hebrew(date: &str) -> Result<HebrewDate, InputError> {
+    let mut v = date
+        .split(|x: char| x == '-' || x == '_' || x == '/' || x == ' ')
+        .collect::<Vec<&str>>();
+    if v.len() != 3 {
+        return Err(InputError::WrongAmntHebDateOptions);
+    }
+    let hebrew_month = v
+        .iter()
+        .map(|x| String::from(*x))
+        .enumerate()
+        .filter_map(|(a, x)| {
+            if HebrewMonth::month_list()
+                .iter()
+                .filter(|y| x.to_uppercase() == String::from(**y).to_uppercase())
+                .count()
+                == 1
+            {
+                Some((a, x))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<(usize, String)>>();
+    if hebrew_month.len() == 0 {
+        return Err(InputError::NotEnoughMonths);
+    } else if hebrew_month.len() > 1 {
+        return Err(InputError::TooManyMonths);
+    }
+    v.remove(hebrew_month[0].0);
+    let int = v
+        .iter()
+        .filter_map(|x| atoi::<i64>(x.as_bytes()))
+        .collect::<Vec<i64>>();
+    let (days, years) = if int[0] > 100 {
+        (int[1], int[0])
+    } else {
+        (int[0], int[1])
+    };
+    HebrewDate::from_ymd(
+        years,
+        HebrewMonth::try_from(&(hebrew_month[0].1)).unwrap(),
+        days,
+    )
+    .map_err(|x| InputError::ConversionError(x))
+}
+fn convert_date_to_fuzzy_gregorian(
+    date: &str,
+) -> Result<chrono::DateTime<Utc>, chrono_english::DateError> {
+    use chrono::{DateTime, NaiveDateTime, Utc};
+    use chrono_english::{parse_date_string, Dialect};
+    use std::time::{SystemTime, UNIX_EPOCH};
+    parse_date_string(
+        date,
+        DateTime::<Utc>::from_utc(
+            NaiveDateTime::from_timestamp(
+                {
+                    let start = SystemTime::now();
+                    let time = start.duration_since(UNIX_EPOCH).unwrap();
+                    time.as_secs() as i64
+                },
+                0,
+            ),
+            Utc,
+        ),
+        Dialect::Us,
+    )
+}
+
+enum InputError {
+    DateFormatError,
+    TooManyMonths,
+    NotEnoughMonths,
+    WrongAmntHebDateOptions,
+    ConversionError(ConversionError),
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 #[macro_use]
 extern crate lazy_static;
 extern crate chrono;
 extern crate time;
-//use chrono::offset::LocalResult;
 use chrono::prelude::*;
 use std::collections::HashMap;
+use std::fmt;
 use time::Duration;
 #[macro_use]
 extern crate enum_primitive;
@@ -19,9 +22,13 @@ const LEAP_YEARS: [bool; 19] = [
     false, false, true, false, false, true, false, true, false, false, true, false, false, true,
     false, false, true, false, true,
 ];
-//var Epoch = time.Date(1826, time.September, 30, 18, 0, 0, 0, time.UTC)
-// FirstMolad (of Tishrei 5587) was on Sunday, October 1st at 17:09:12 Chalakim
-const FIRST_MOLAD: i64 = 23 * 1080 + (9 * 1080 / 60) + 12;
+//var Epoch = time.Date(1, time.September, 30, 18, 0, 0, 0, time.UTC)
+// FirstMolad (of Tishrei 3672) was on Sunday, October 1st at 17:09:12 Chalakim
+const FIRST_MOLAD: i64 = 2 * 24 * 1080 + 20 * 1080 + (43 * 1080 / 60) - 11;
+const FIRST_YEAR: i64 = 3762;
+lazy_static! {
+    static ref EPOCH: chrono::DateTime<Utc> = Utc.ymd(1, 9, 4).and_hms(18, 0, 0);
+}
 
 lazy_static! {
     static ref YEAR_SCHED: HashMap<i64, [i8; 14]> = {
@@ -35,10 +42,6 @@ lazy_static! {
         m.insert(385, [30, 30, 30, 29, 30, 0, 30, 29, 30, 29, 30, 29, 30, 29]);
         m
     };
-}
-
-lazy_static! {
-    static ref EPOCH: chrono::DateTime<Utc> = Utc.ymd(1826, 9, 30).and_hms(18, 0, 0);
 }
 
 lazy_static! {
@@ -56,13 +59,13 @@ lazy_static! {
 }
 
 fn get_molad_for_year(year: i64) -> i64 {
-    let amnt_of_cycles = (year - 5587) / 19;
+    let amnt_of_cycles = (year - FIRST_YEAR) / 19;
 
     //full_cycle_chalakim = (7 full years)(13 month/year)(ChalakimBetweenMolad) + (12 short years)(12 months/year)(ChalakimBetweenMolad)
     let full_cycle_chalakim = 7 * 13 * CHALAKIM_BETWEEN_MOLAD + 12 * 12 * CHALAKIM_BETWEEN_MOLAD;
 
     let mut amnt_chalakim = full_cycle_chalakim * amnt_of_cycles;
-    let cur_year_in_cycle = (year - 5587) % 19;
+    let cur_year_in_cycle = (year - FIRST_YEAR) % 19;
     for i in 0..cur_year_in_cycle {
         amnt_chalakim += months_per_year(i) * CHALAKIM_BETWEEN_MOLAD;
     }
@@ -104,13 +107,13 @@ fn get_rosh_hashana(year: i64) -> i64 {
     if !reg_postpone
         && dow == 2
         && amnt_chalakim > 9 * CHALAKIM_PER_HOUR + 204
-        && months_per_year((year - 5587) % 19) == 12
+        && months_per_year((year - FIRST_YEAR) % 19) == 12
     {
         amnt_days += 2;
     }
 
     if !reg_postpone
-        && months_per_year((year - 5587 - 1) % 19) == 13
+        && months_per_year((year - FIRST_YEAR - 1) % 19) == 13
         && dow == 1
         && amnt_chalakim > 12 * CHALAKIM_PER_HOUR + 3 * CHALAKIM_PER_HOUR + 589
     {
@@ -125,8 +128,9 @@ fn get_hebrew_date(p: chrono::DateTime<Utc>) -> (HebrewMonth, i64, i64) {
     let diff_sec = (p - *EPOCH).num_seconds();
     let diff_chalakim = diff_sec * CHALAKIM_PER_HOUR / 60 / 60;
     let amnt_cycles = diff_chalakim / amnt_chalakim_per_cycle;
+
     let mut remainder_chalakim = diff_chalakim % amnt_chalakim_per_cycle;
-    let mut year = 5587 + amnt_cycles * 19;
+    let mut year = FIRST_YEAR + amnt_cycles * 19;
     for i in LEAP_YEARS.iter() {
         let chalakim_this_year = if *i { 13 } else { 12 } * CHALAKIM_BETWEEN_MOLAD;
 
@@ -152,18 +156,18 @@ fn get_hebrew_date(p: chrono::DateTime<Utc>) -> (HebrewMonth, i64, i64) {
     let day = if p.hour() <= 18 { 0 } else { 1 };
     (
         HebrewMonth::from_i32(month).unwrap(),
-        day + remainder_chalakim / (CHALAKIM_PER_HOUR * 24),
+        day + remainder_chalakim / (CHALAKIM_PER_HOUR * 24) + 1,
         year,
     )
 }
 
 fn get_english_date(h: HebrewDate) -> Result<chrono::DateTime<Utc>, ConversionError> {
-    if months_per_year((h.year - 5587) % 19) == 12
+    if months_per_year((h.year - FIRST_YEAR) % 19) == 12
         && (h.month == HebrewMonth::Adar1 || h.month == HebrewMonth::Adar2)
     {
         return Err(ConversionError::IsNotLeapYear);
     }
-    if months_per_year((h.year - 5587) % 19) == 13 && h.month == HebrewMonth::Adar {
+    if months_per_year((h.year - FIRST_YEAR) % 19) == 13 && h.month == HebrewMonth::Adar {
         return Err(ConversionError::IsLeapYear);
     }
 
@@ -185,8 +189,8 @@ fn get_english_date(h: HebrewDate) -> Result<chrono::DateTime<Utc>, ConversionEr
     Ok(*EPOCH + Duration::days(amnt_days))
 }
 enum_from_primitive! {
-#[derive(Debug, PartialEq, Copy, Clone)]
-pub enum HebrewMonth {
+  #[derive(Debug, PartialEq, Copy, Clone, Deserialize, Serialize)]
+  pub enum HebrewMonth {
     Tishrei = 0,
     Cheshvan = 1,
     Kislev = 2,
@@ -201,7 +205,65 @@ pub enum HebrewMonth {
     Tammuz = 11,
     Av = 12,
     Elul = 13
+  }
 }
+
+impl HebrewMonth {
+    pub fn month_list() -> Vec<&'static str> {
+        vec![
+            "Tishrei", "Cheshvan", "Kislev", "Teves", "Shvat", "Adar", "Adar1", "Adar2", "Nissan",
+            "Iyar", "Sivan", "Tammuz", "Av", "Elul",
+        ]
+    }
+    pub fn try_from(s: &str) -> Result<HebrewMonth, ConversionError> {
+        match s {
+            "Tishrei" => Ok(HebrewMonth::Tishrei),
+            "Cheshvan" => Ok(HebrewMonth::Cheshvan),
+            "Kislev" => Ok(HebrewMonth::Kislev),
+            "Teves" => Ok(HebrewMonth::Teves),
+            "Shvat" => Ok(HebrewMonth::Shvat),
+            "Adar" => Ok(HebrewMonth::Adar),
+            "Adar1" => Ok(HebrewMonth::Adar1),
+            "Adar 1" => Ok(HebrewMonth::Adar1),
+            "Adar Aleph" => Ok(HebrewMonth::Adar1),
+            "Adar2" => Ok(HebrewMonth::Adar2),
+            "Adar 2" => Ok(HebrewMonth::Adar2),
+            "Adar Beis" => Ok(HebrewMonth::Adar2),
+            "Nissan" => Ok(HebrewMonth::Nissan),
+            "Iyar" => Ok(HebrewMonth::Iyar),
+            "Sivan" => Ok(HebrewMonth::Sivan),
+            "Tammuz" => Ok(HebrewMonth::Tammuz),
+            "Av" => Ok(HebrewMonth::Av),
+            "Elul" => Ok(HebrewMonth::Elul),
+            _ => Err(ConversionError::MonthDoesntExist),
+        }
+    }
+
+    pub fn as_str(&self) -> &str {
+        match self {
+            HebrewMonth::Tishrei => "Tishrei",
+            HebrewMonth::Cheshvan => "Cheshvan",
+            HebrewMonth::Kislev => "Kislev",
+            HebrewMonth::Teves => "Teves",
+            HebrewMonth::Shvat => "Shvat",
+            HebrewMonth::Adar => "Adar",
+            HebrewMonth::Adar1 => "Adar 1",
+            HebrewMonth::Adar2 => "Adar 2",
+            HebrewMonth::Nissan => "Nissan",
+            HebrewMonth::Iyar => "Iyar",
+            HebrewMonth::Sivan => "Sivan",
+            HebrewMonth::Tammuz => "Tammuz",
+            HebrewMonth::Av => "Av",
+            HebrewMonth::Elul => "Elul",
+        }
+    }
+}
+
+impl std::fmt::Display for HebrewMonth {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let string = self.as_str();
+        write!(f, "{}", string)
+    }
 }
 
 #[derive(Debug)]
@@ -211,15 +273,52 @@ pub struct HebrewDate {
     year: i64,
 }
 
-
-impl From<chrono::DateTime<Utc>> for HebrewDate{
-    fn from(time: chrono::DateTime<Utc>) -> Self {
-        let res = get_hebrew_date(time);
-        HebrewDate{month: res.0, day: res.1 as i8, year: res.2}
+impl HebrewDate {
+    pub fn day(&self) -> i8 {
+        return self.day;
     }
-}
-pub fn try_from(h: HebrewDate) -> Result<chrono::DateTime<Utc>, ConversionError> {
-    get_english_date(h)
+    pub fn month(&self) -> HebrewMonth {
+        return self.month;
+    }
+    pub fn year(&self) -> i64 {
+        return self.year;
+    }
+
+    pub fn from_ymd(
+        year: i64,
+        month: HebrewMonth,
+        day: i64,
+    ) -> Result<HebrewDate, ConversionError> {
+        if year < FIRST_YEAR + 1 {
+            return Err(ConversionError::YearTooSmall(format!(
+                "We cannot work with Gregorian years before year {}",
+                FIRST_YEAR + 1
+            )));
+        }
+        Ok(HebrewDate {
+            year: year,
+            month: month,
+            day: day as i8,
+        })
+    }
+
+    pub fn from_eng(time: chrono::DateTime<Utc>) -> Result<HebrewDate, ConversionError> {
+        if time.year() < (*EPOCH + Duration::days(365)).year() {
+            return Err(ConversionError::YearTooSmall(format!(
+                "We cannot work with Gregorian years before year {}",
+                (*EPOCH + Duration::days(365)).year()
+            )));
+        }
+        let res = get_hebrew_date(time);
+        Ok(HebrewDate {
+            month: res.0,
+            day: res.1 as i8,
+            year: res.2,
+        })
+    }
+    pub fn to_eng(self) -> Result<chrono::DateTime<Utc>, ConversionError> {
+        get_english_date(self)
+    }
 }
 
 #[derive(Debug)]
@@ -228,4 +327,19 @@ pub enum ConversionError {
     IsNotLeapYear,
     TooManyDaysInMonth(i8),
     IsLeapYear,
+    MonthDoesntExist,
+    YearTooSmall(String),
+}
+
+impl std::fmt::Display for ConversionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use crate::ConversionError::*;
+        match self {
+        IsNotLeapYear => write!(f, "The year you specified is not a leap year, yet you're trying to convert from an Adar1 or Adar2. Use the regular Adar for a regular year"),
+        TooManyDaysInMonth(d) => write!(f,"There aren't {} days in this month",d),
+        IsLeapYear => write!(f, "The year you specified is a leap year, yet you're trying to convert from a Regular Adar. Use Adar1 or Adar2 on a leap year"),
+        MonthDoesntExist => write!(f, "This month doesn't exist. Please specify another one."),
+        YearTooSmall(s) => write!(f, "{}",s)
+        }
+    }
 }
