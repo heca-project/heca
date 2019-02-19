106e4740dff4287c204147d6fb3423196ac81293
I picked a name <ipickedaname@github.com>
2019-02-19 02:17:26 +0000

    reformatted

--- lib/heca-lib/src/convert/year.rs
+++ lib/heca-lib/src/convert/year.rs
@@ -1,50 +1,53 @@
-use crate::convert::*;
 use crate::convert;
-use crate::types::*;
-use crate::holidays::*;
+use crate::convert::*;
 use crate::holidays::get_torah_reading_days_list;
+use crate::holidays::*;
+use crate::types::*;
 
-pub struct HebrewYear{
+pub struct HebrewYear {
     year: u64,
 }
 
 impl HebrewYear {
-
     #[inline]
-    pub fn new(year: u64) -> Result<HebrewYear,ConversionError> {
+    pub fn new(year: u64) -> Result<HebrewYear, ConversionError> {
         if year < convert::FIRST_YEAR {
             Err(ConversionError::YearTooSmall)
-        }else{
-            Ok(HebrewYear{year})
+        } else {
+            Ok(HebrewYear { year })
         }
     }
 
     #[inline]
-    pub fn add_year(&mut self){
-        self.year+=1;
+    pub fn add_year(&mut self) {
+        self.year += 1;
     }
     #[inline]
-    pub fn year(&self) ->u64 {
+    pub fn year(&self) -> u64 {
         self.year
     }
-    pub fn to_hebrew_date(&self, month: HebrewMonth, day: u8) -> Result<HebrewDate,ConversionError> {
-        HebrewDate::from_ymd(self.year,month,day)
+    pub fn to_hebrew_date(
+        &self,
+        month: HebrewMonth,
+        day: u8,
+    ) -> Result<HebrewDate, ConversionError> {
+        HebrewDate::from_ymd(self.year, month, day)
     }
 
-    pub fn get_holidays(&self, yt_type: YomTovType) -> Vec<SpecialDay>{
+    pub fn get_holidays(&self, yt_type: YomTovType) -> Vec<SpecialDay> {
         match yt_type {
             YomTovType::YomTov => get_yt_list(self.year),
             YomTovType::SpecialTorahReading => get_torah_reading_days_list(self.year),
             YomTovType::RegularTorahReading => vec![SpecialDay {
-            day: HebrewDate::from_ymd(self.year, HebrewMonth::Tishrei, 30).unwrap(),
-            name: "Rosh Chodesh Cheshvan 1".into()}]
+                day: HebrewDate::from_ymd(self.year, HebrewMonth::Tishrei, 30).unwrap(),
+                name: "Rosh Chodesh Cheshvan 1".into(),
+            }],
         }
     }
 }
 
-pub enum YomTovType{
+pub enum YomTovType {
     YomTov,
     SpecialTorahReading,
     RegularTorahReading,
-
 }
