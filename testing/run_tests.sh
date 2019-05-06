d401a3f4303d4ed98df9a42416e51945e0bb8ab8
I picked a name <ipickedaname@mail.com>
2019-05-06 01:33:35 +0000

    added testing
    Signed-off-by: I picked a name <ipickedaname@mail.com>

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -2,7 +2,9 @@
 
 set -e
 set -o pipefail
-
+cd ../lib/heca-lib
+cargo test
+cd ../../testing
 cargo build --release 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $DIR/convert_test.sh
