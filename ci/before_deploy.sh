596dd2ab1c15eb49e442205199022aa76c75e89f
I picked a name <ipickedaname@mail.com>
2019-12-04 00:25:58 -0800

    possibly fixed xz bug

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -6,5 +6,5 @@ strip "/tmp/heca/$TARGET/release/heca"
 name="${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
 mkdir -p /tmp/heca-staging
 cp "/tmp/heca/$TARGET/release/heca" "/tmp/heca-staging/$name"
-xz "/tmp/heca/$TARGET/release/heca"
-cp "/tmp/heca/$TARGET/release/heca.xz" "/tmp/heca-staging/$name.xz"
+xz "/tmp/heca-staging/$name"
+cp "/tmp/heca/$TARGET/release/heca" "/tmp/heca-staging/$name"
