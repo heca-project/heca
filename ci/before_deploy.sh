e05557f65972e939f9f65137ccc35711fb231469
I picked a name <ipickedaname@mail.com>
2019-12-04 01:43:23 -0800

    cherrypick travis

---
+++ ci/before_deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+cargo build --target "$TARGET" --release --target-dir=/tmp/heca
+cargo publish
+strip "/tmp/heca/$TARGET/release/heca"
+name="heca-${TRAVIS_TAG}-${TARGET}"
+mkdir -p /tmp/heca-staging
+cp "/tmp/heca/$TARGET/release/heca" "/tmp/heca-staging/$name"
+xz "/tmp/heca-staging/$name"
+cp "/tmp/heca/$TARGET/release/heca" "/tmp/heca-staging/$name"
