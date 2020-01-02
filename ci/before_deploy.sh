16dda1c67a1d82ecb0abc1d8427031f777b2b03a
I picked a name <ipickedaname@mail.com>
2020-01-01 21:58:00 -0800

    Tried fixing result size

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -4,13 +4,13 @@ set -e
 cargo build --target "$TARGET" --release --target-dir=/tmp/heca
 
 set +e
-cargo publish
+#cargo publish
 
 set -e
 strip "/tmp/heca/$TARGET/release/heca"
 name="heca-${TRAVIS_TAG}-${TARGET}"
+mkdir -p /tmp/heca-staging-1
 mkdir -p /tmp/heca-staging
-cp "/tmp/heca/$TARGET/release/heca" "/tmp/heca-staging/$name"
-xz "/tmp/heca-staging/$name"
-rm "/tmp/heca-staging/$name"
-cp "/tmp/heca/$TARGET/release/heca" "/tmp/heca-staging/$name"
+cp "/tmp/heca/$TARGET/release/heca" "/tmp/heca-staging-1/$name"
+xz "/tmp/heca-staging-1/$name"
+cp "/tmp/heca-staging-1/$name.xz /tmp/heca-staging/"
