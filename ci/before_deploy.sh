d9174499d95569e4933db8114a66203129b3ff5d
I picked a name <ipickedaname@mail.com>
2020-01-10 10:59:10 -0800

    Added windows build

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -12,6 +12,10 @@ name="heca-${TRAVIS_TAG}-${TARGET}"
 mkdir -p /tmp/heca-staging-1
 mkdir -p /tmp/heca-staging
 cp "/tmp/heca/$TARGET/release/heca" "/tmp/heca-staging-1/$name"
+
+set +e
 xz "/tmp/heca-staging-1/$name"
 cp "/tmp/heca-staging-1/$name.xz" "/tmp/heca-staging/"
+
+set -e
 cp "/tmp/heca/$TARGET/release/heca" "/tmp/heca-staging/$name"
